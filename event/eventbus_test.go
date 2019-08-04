b8d6ebee87d4f42d108521f650144cf978185432
ericliu2010 <ericliu2010@qq.com>
2019-08-04 22:57:21 +0800

    add:添加测试的3方EventBus

---
+++ event/eventbus_test.go
@@ -0,0 +1,15 @@
+package event
+
+// go get github.com/asaskevich/EventBus
+import (
+	"fmt"
+	"testing"
+)
+
+func TestEventBus(t *testing.T) {
+	fmt.Println("")
+}
+
+func BenchmarkEventBus(b *testing.B) {
+
+}
