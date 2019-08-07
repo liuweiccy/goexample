03f2101457660f59342b482bf52db845c45b0b41
liuwei2 <liuwei2@digisky.com>
2019-08-07 12:05:51 +0800

    fix:优化战国登录服的测试程序

--- net/http/login_test.go
+++ net/http/login_test.go
@@ -1,34 +1,51 @@
 package http
 
 import (
-	"bytes"
-	"io/ioutil"
+	"fmt"
 	"net/http"
+	"net/url"
+	"sync"
 	"testing"
+	"time"
 )
 
-func TestLogin(t *testing.T) {
-	param := "{\"clientType\":\"1\",\"account\":\"4735744240362690\",\"type\":\"1\",\"token\":\"66684A8B3733498AA5289D068DA96637\"}"
+// 访问总数
+const N int = 10000
 
-	response, err := http.Post("http://192.168.20.97:8081/ws/logininfo", "application/json;charset=utf-8", bytes.NewBuffer([]byte(param)))
-	if err != nil {
-		t.Error("请求http地址发生错误：")
+// 并发数
+const C int = 1000
+
+func TestLogin(t *testing.T) {
+	group := &sync.WaitGroup{}
+	group.Add(N)
+
+	s := time.Now()
+	for i := 0; i < C; i++ {
+		go func() {
+			for i := 0; i < N/C; i++ {
+				testRequest(group)
+			}
+		}()
 	}
 
-	defer response.Body.Close()
+	group.Wait()
 
-	body, _ := ioutil.ReadAll(response.Body)
-	t.Log(string(body))
+	fmt.Printf("请求%d次总共花费时间：%d ms, QPS:%f \n", N, time.Now().Sub(s).Milliseconds(), float64(N)/time.Now().Sub(s).Seconds())
 }
 
-func BenchmarkLogin(b *testing.B) {
-	response, err := http.Get("http://192.168.20.97:8081/ws/logininfo")
-	if err != nil {
-		b.Error("请求http地址发生错误：")
-	}
-
-	defer response.Body.Close()
-
-	body, _ := ioutil.ReadAll(response.Body)
-	b.Log(string(body))
+func testRequest(group *sync.WaitGroup) {
+	args := url.Values{}
+	args.Set("clientType", "1")
+	args.Set("account", "4735744240362690")
+	args.Set("type", "1")
+	args.Set("token", "66684A8B3733498AA5289D068DA96637")
+	_, _ = http.PostForm("http://192.168.20.97:8081/ws/logininfo", args)
+	group.Done()
+	//response, err := http.PostForm("http://192.168.20.97:8081/ws/logininfo", args)
+	//if err != nil {
+	//	fmt.Println("请求http地址发生错误：")
+	//}
+	// defer response.Body.Close()
+	// body, _ := ioutil.ReadAll(response.Body)
+	// fmt.Println(string(body))
 }
