d61b43444fee2e60d67033397a1b667c5bef4563
ericliu2010 <ericliu2010@qq.com>
2019-09-19 00:05:04 +0800

    add:随机测试&列表测试

--- base/test/word_test.go
+++ base/test/word_test.go
@@ -1,6 +1,10 @@
 package test
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
 
 func TestIsPalindrome(t *testing.T) {
 	if !IsPalindrome("kayak") {
@@ -22,5 +26,56 @@ func TestChinesePalindrome(t *testing.T) {
 	input := "刘威刘"
 	if !IsPalindrome(input) {
 		t.Errorf("IsPalindrome(%q) == false", input)
+	}
+}
+
+func TestIsPalindrome2(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  bool
+	}{
+		{"", true},
+		{"a", true},
+		{"ab", false},
+		{"aba", true},
+		{"aa", true},
+		{"", true},
+		{"", true},
+		{"", true},
+		{"", true},
+		{"", true},
+		{"", true},
+		{"", true},
+		{"", true},
+	}
+
+	for _, test := range tests {
+		if got := IsPalindrome(test.input); got != test.want {
+			t.Errorf("IsPalindrome(%q) = %v\n", test.input, got)
+		}
+	}
+}
+
+func randomPalindrome(rand *rand.Rand) string {
+	n := rand.Intn(25)
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rand.Intn(0x1000))
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+	return string(runes)
+}
+
+func TestIsPalindromeRandom(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Rondom seed: %d\n", seed)
+	rand := rand.New(rand.NewSource(seed))
+
+	for i := 0; i < 1000000; i++ {
+		p := randomPalindrome(rand)
+		if !IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = false\n", p)
+		}
 	}
 }
