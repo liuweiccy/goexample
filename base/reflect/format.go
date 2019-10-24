0ed8785f9b04f966edbb338fc80ca00fcf7575fb
liuwei2 <liuwei2@digisky.com>
2019-10-24 18:27:27 +0800

    add:反射&S表达式

--- base/reflect/format.go
+++ base/reflect/format.go
@@ -1,6 +1,8 @@
 package reflect
 
 import (
+	"bytes"
+	"fmt"
 	"reflect"
 	"strconv"
 )
@@ -26,4 +28,120 @@ func formatAtom(v reflect.Value) string {
 	default:
 		return v.Type().String() + " value"
 	}
+}
+
+func Display(name string, x interface{}) {
+	fmt.Printf("Display %s (%T)\n", name, x)
+	display(name, reflect.ValueOf(x))
+}
+
+func display(path string, v reflect.Value) {
+	switch v.Kind() {
+	case reflect.Invalid:
+		fmt.Printf("%s = invalid\n", path)
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < v.Len(); i++ {
+			display(fmt.Sprintf("%s[%d]", path, i), v.Index(i))
+		}
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
+			filedPath := fmt.Sprintf("%s.%s", path, v.Type().Field(i).Name)
+			display(filedPath, v.Field(i))
+		}
+	case reflect.Map:
+		for _, key := range v.MapKeys() {
+			display(fmt.Sprintf("%s[%s]", path, formatAtom(key)), v.MapIndex(key))
+		}
+	case reflect.Ptr:
+		if v.IsNil() {
+			fmt.Printf("%s = nil\n", path)
+		} else {
+			display(fmt.Sprintf("(*%s)", path), v.Elem())
+		}
+	case reflect.Interface:
+		if v.IsNil() {
+			fmt.Printf("%s = nil\n", path)
+		} else {
+			fmt.Printf("%s.type = %s\n", path, v.Elem().Type())
+			display(path+".value", v.Elem())
+		}
+	default:
+		fmt.Printf("%s = %s\n", path, formatAtom(v))
+	}
+}
+
+func encode(buf *bytes.Buffer, v reflect.Value) error {
+	switch v.Kind() {
+	case reflect.Invalid:
+		buf.WriteString("nil")
+
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		fmt.Fprintf(buf, "%d", v.Int())
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		fmt.Fprintf(buf, "%d", v.Uint())
+
+	case reflect.String:
+		buf.WriteString(v.String())
+
+	case reflect.Ptr:
+		return encode(buf, v.Elem())
+
+	case reflect.Array, reflect.Slice:
+		buf.WriteString("(")
+		for i := 0; i < v.Len(); i++ {
+			if i > 0 {
+				buf.WriteString(" ")
+			}
+			if err := encode(buf, v.Index(i)); err != nil {
+				return err
+			}
+		}
+		buf.WriteString(")")
+
+	case reflect.Struct:
+		buf.WriteString("(")
+		for i := 0; i < v.NumField(); i++ {
+			if i > 0 {
+				buf.WriteString(" ")
+			}
+			fmt.Fprintf(buf, "(%s ", v.Type().Field(i).Name)
+			if err := encode(buf, v.Field(i)); err != nil {
+				return err
+			}
+			buf.WriteString(")")
+		}
+		buf.WriteString(")")
+
+	case reflect.Map:
+		buf.WriteString("(")
+		for i, key := range v.MapKeys() {
+			if i > 0 {
+				buf.WriteString(" ")
+			}
+			buf.WriteString("(")
+			if err := encode(buf, key); err != nil {
+				return err
+			}
+			buf.WriteString(" ")
+			if err := encode(buf, v.MapIndex(key)); err != nil {
+				return err
+			}
+			buf.WriteString(")")
+		}
+		buf.WriteString(")")
+
+	default:
+		return fmt.Errorf("Unsupported type:%s\n", v.Type())
+	}
+
+	return nil
+}
+
+func Marshal(v interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	if err := encode(&buf, reflect.ValueOf(v)); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }
--- base/reflect/format_test.go
+++ base/reflect/format_test.go
@@ -2,17 +2,101 @@ package reflect
 
 import (
 	"fmt"
+	"os"
+	"reflect"
 	"testing"
 	"time"
 )
 
 func TestAny(t *testing.T) {
 	var x int64 = 1
-	var d time.Duration = time.Second
+	var d = time.Second
 
 	fmt.Println(Any(x))
 	fmt.Println(Any(d))
 
 	fmt.Println(Any([]int64{x}))
 	fmt.Println(Any([]time.Duration{d}))
+}
+
+type Movie struct {
+	Title, Subtitle string
+	Year            int
+	Color           bool
+	Actor           map[string]string
+	Oscars          []string
+	Sequel          *string
+}
+
+var strangelove = Movie{
+	Title:    "大话西游",
+	Subtitle: "大圣娶亲",
+	Year:     1995,
+	Color:    true,
+	Actor: map[string]string{
+		"周星驰": "主角",
+		"吴孟达": "主角",
+		"朱茵":  "主角",
+		"蔡少芬": "配角",
+		"蓝洁瑛": "配角",
+		"莫文蔚": "配角",
+		"罗家英": "配角",
+		"陆树铭": "友情出演",
+		"李健仁": "友情出演",
+	},
+	Oscars: []string{"最佳搞笑", "最佳主角", "最佳剧情"},
+	Sequel: nil,
+}
+
+func TestDisplay(t *testing.T) {
+
+	Display("大话西游", strangelove)
+
+	//Display("os.Stderr", os.Stderr)
+	Display("os.Stderr", reflect.ValueOf(os.Stderr))
+}
+
+func TestDisplay2(t *testing.T) {
+	var i interface{} = 3
+	Display("i", i)
+
+	Display("&i", &i)
+}
+
+type Cycle struct {
+	Value int
+	Tail  *Cycle
+}
+
+// 存在循环引用的情况，会首尾相连，输出无穷无尽
+func TestDisplay3(t *testing.T) {
+	var c Cycle
+	c = Cycle{42, &c}
+	Display("c", c)
+}
+
+type Student struct {
+	Name  string
+	Age   int
+	Grade []int
+	elder map[string]string
+}
+
+var stu = Student{
+	Name:  "Eric",
+	Age:   29,
+	Grade: []int{560, 560, 580},
+	elder: map[string]string{
+		"爸爸":  "VV",
+		"妈妈":  "YY",
+		"爷爷":  "LHX",
+		"奶奶":  "LLX",
+		"外公":  "CMX",
+		"曾祖母": "CSF",
+	},
+}
+
+func TestMarshal(t *testing.T) {
+	bytearr, _ := Marshal(stu)
+	fmt.Printf("%s\n", string(bytearr))
 }
